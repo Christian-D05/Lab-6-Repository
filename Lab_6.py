cb65650de3b281cea182730d73378656a82d91ef
APCSAemily <emilyyqhuang@gmail.com>
2024-10-22 12:33:14 -0500

    Updated decoder() method. I also updated the menu option.

--- Lab_6.py
+++ Lab_6.py
@@ -13,12 +13,31 @@ def encoder(password):
             encoded_password += str(int(temp_list[i]) + 3)
     return encoded_password
 
+#Added by Yiqiao (Emily) Huang
+def decoder(password_to_decode):
+    decoded_pass_list = []
+    for letter in password_to_decode:
+        decoded_pass_list.append(str((int(letter) - 3 + 10) % 10))
+    return "".join(decoded_pass_list)
+
+
 if __name__ == '__main__':
-    while True:
+    is_running = True
+    while is_running:
         print("Menu \n------------- \n1. Encode \n2. Decode \n3. Quit \n")
         menu_option = int(input("Please enter an option: "))
         if menu_option == 1:
             password = str(input("Please enter your password to encode: "))
             encoded_password = encoder(password)
             print("Your password has been encoded and stored!\n")
+        #Option 2 added by Yiqiao (Emily) Huang
+        if menu_option == 2:
+            decoded_password = decoder(encoded_password)
+            print(f"Your encoded password is {encoded_password}, and the original password is {decoded_password}")
+            print()
+        #Option 3 added by Yiqiao (Emily Huang
+        if menu_option == 3:
+            is_running = False
+
+
 
